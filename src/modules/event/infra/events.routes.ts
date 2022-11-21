4002d73b199f6e86dcdc2b58ca79ed4fc355af31
João Guilherme Fonseca <joao.g.f@icloud.com>
2022-11-21 09:54:02 -0300

    fix

--- src/modules/event/infra/events.routes.ts
+++ src/modules/event/infra/events.routes.ts
@@ -7,6 +7,7 @@ import { FindAllEventsController } from '../useCases/FindAllEvents/FindAllEvents
 import { FindEventsImInController } from '../useCases/FindEventsImIn/FindEventsImInController';
 import { FindMyEventsController } from '../useCases/FindMyEvents/FindMyEventsController';
 import { UpdateEventController } from '../useCases/UpdateEvent/UpdateEventController';
+import { RemoveTeamFromEventController } from '../useCases/RemoveTeamFromEvent/RemoveTeamFromEventController';
 
 export const eventsRouter = Router();
 
@@ -17,9 +18,11 @@ const findMyEventsController = new FindMyEventsController();
 const findEventsImInController = new FindEventsImInController();
 const updateEventController = new UpdateEventController();
 const deleteEventController = new DeleteEventController();
+const removeTeamFromEventController = new RemoveTeamFromEventController();
 
 eventsRouter.post('/', createEventController.handle);
 eventsRouter.post('/addTeam', addTeamToEventController.handle);
+eventsRouter.post('/removeTeam', removeTeamFromEventController.handle);
 eventsRouter.get('/', findAllEventsController.handle);
 eventsRouter.get('/myEvents/:id', findMyEventsController.handle);
 eventsRouter.get('/eventsImIn/:id', findEventsImInController.handle);
